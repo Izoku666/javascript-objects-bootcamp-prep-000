c6aaf7e5a12468d48594d621eefd4685125b954f
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-13 02:42:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 var playlist = {
+<<<<<<< HEAD
   "Slowdive" : 'Alison',
   "My Bloody Valentine" : 'Sometimes'
 };
@@ -14,4 +15,20 @@ function removeFromPlaylist(obj , artistName)
 {
   delete obj[artistName];
   return obj;
+=======
+  //artist : "song",
+    "My Bloody Valentine" : 'Sometimes',
+  "Slowdive" : 'Alison'
+};
+
+function updatePlaylist(obj , name , song)  // Playlist pl = new Playlist();
+{
+  obj[name] = song
+  return obj
+}
+
+function removeFromPlaylist(obj , name)
+{
+  
+>>>>>>> 12af41aa7c3ab74d9cbf308fc16bc1bab04b13da
 }
